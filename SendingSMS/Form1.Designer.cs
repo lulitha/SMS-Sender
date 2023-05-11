dbcf65f31d72e78f1c1a8eec452926eec8d78bbf
lulitha gihan <lulitha.info@gmail.com>
2023-05-11 21:22:07 +0530

    Add project files.

---
+++ SendingSMS/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace SendingSMS
+{
+    partial class SMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SMS));
+            this.ComPort = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblStatus = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.btnClose = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2GradientButton2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.btnConnect = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.txtNumber = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtSMS = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2GradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ComPort
+            // 
+            this.ComPort.BackColor = System.Drawing.Color.Transparent;
+            this.ComPort.BorderColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ComPort.BorderRadius = 15;
+            this.ComPort.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComPort.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ComPort.FocusedColor = System.Drawing.Color.Empty;
+            this.ComPort.FocusedState.Parent = this.ComPort;
+            this.ComPort.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ComPort.ForeColor = System.Drawing.Color.Silver;
+            this.ComPort.FormattingEnabled = true;
+            this.ComPort.HoverState.Parent = this.ComPort;
+            this.ComPort.ItemHeight = 30;
+            this.ComPort.ItemsAppearance.Parent = this.ComPort;
+            this.ComPort.Location = new System.Drawing.Point(86, 58);
+            this.ComPort.Name = "ComPort";
+            this.ComPort.ShadowDecoration.Parent = this.ComPort;
+            this.ComPort.Size = new System.Drawing.Size(140, 36);
+            this.ComPort.TabIndex = 12;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(40, 66);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(40, 18);
+            this.guna2HtmlLabel1.TabIndex = 14;
+            this.guna2HtmlLabel1.Text = "PORT :";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblStatus.Location = new System.Drawing.Point(40, 116);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(100, 18);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "NOT CONNECTED";
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.Controls.Add(this.btnClose);
+            this.guna2GradientPanel1.Controls.Add(this.guna2HtmlLabel2);
+            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(198)))), ((int)(((byte)(102)))));
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(30)))), ((int)(((byte)(119)))));
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.ShadowDecoration.Parent = this.guna2GradientPanel1;
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(438, 30);
+            this.guna2GradientPanel1.TabIndex = 16;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Image = global::smsSender.Properties.Resources.Close;
+            this.btnClose.Location = new System.Drawing.Point(397, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnClose.ShadowDecoration.Parent = this.btnClose;
+            this.btnClose.Size = new System.Drawing.Size(36, 33);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnClose.TabIndex = 18;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.Black;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(40, 6);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(82, 19);
+            this.guna2HtmlLabel2.TabIndex = 14;
+            this.guna2HtmlLabel2.Text = "SMS SENDER";
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 15;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2GradientButton2
+            // 
+            this.guna2GradientButton2.BorderRadius = 20;
+            this.guna2GradientButton2.CheckedState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.CustomImages.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(198)))), ((int)(((byte)(102)))));
+            this.guna2GradientButton2.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold);
+            this.guna2GradientButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton2.HoverState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Location = new System.Drawing.Point(216, 354);
+            this.guna2GradientButton2.Name = "guna2GradientButton2";
+            this.guna2GradientButton2.ShadowDecoration.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Size = new System.Drawing.Size(180, 45);
+            this.guna2GradientButton2.TabIndex = 17;
+            this.guna2GradientButton2.Text = "SEND";
+            this.guna2GradientButton2.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BorderRadius = 15;
+            this.btnConnect.CheckedState.Parent = this.btnConnect;
+            this.btnConnect.CustomImages.Parent = this.btnConnect;
+            this.btnConnect.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(198)))), ((int)(((byte)(102)))));
+            this.btnConnect.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold);
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.HoverState.Parent = this.btnConnect;
+            this.btnConnect.Image = global::smsSender.Properties.Resources.antenna;
+            this.btnConnect.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnConnect.Location = new System.Drawing.Point(232, 58);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.ShadowDecoration.Parent = this.btnConnect;
+            this.btnConnect.Size = new System.Drawing.Size(164, 36);
+            this.btnConnect.TabIndex = 17;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BorderColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtNumber.BorderRadius = 15;
+            this.txtNumber.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNumber.DefaultText = "";
+            this.txtNumber.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtNumber.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtNumber.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNumber.DisabledState.Parent = this.txtNumber;
+            this.txtNumber.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNumber.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.txtNumber.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNumber.FocusedState.Parent = this.txtNumber;
+            this.txtNumber.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNumber.HoverState.Parent = this.txtNumber;
+            this.txtNumber.IconLeft = global::smsSender.Properties.Resources.mobile_phone;
+            this.txtNumber.Location = new System.Drawing.Point(40, 137);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.PasswordChar = '\0';
+            this.txtNumber.PlaceholderText = "MOBILE NUMBER";
+            this.txtNumber.SelectedText = "";
+            this.txtNumber.ShadowDecoration.Parent = this.txtNumber;
+            this.txtNumber.Size = new System.Drawing.Size(356, 36);
+            this.txtNumber.TabIndex = 13;
+            // 
+            // txtSMS
+            // 
+            this.txtSMS.BorderColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtSMS.BorderRadius = 15;
+            this.txtSMS.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSMS.DefaultText = "";
+            this.txtSMS.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtSMS.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtSMS.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSMS.DisabledState.Parent = this.txtSMS;
+            this.txtSMS.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSMS.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.txtSMS.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSMS.FocusedState.Parent = this.txtSMS;
+            this.txtSMS.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSMS.HoverState.Parent = this.txtSMS;
+            this.txtSMS.IconLeft = global::smsSender.Properties.Resources.sms;
+            this.txtSMS.Location = new System.Drawing.Point(40, 179);
+            this.txtSMS.Multiline = true;
+            this.txtSMS.Name = "txtSMS";
+            this.txtSMS.PasswordChar = '\0';
+            this.txtSMS.PlaceholderText = "MESSAGE";
+            this.txtSMS.SelectedText = "";
+            this.txtSMS.ShadowDecoration.Parent = this.txtSMS;
+            this.txtSMS.Size = new System.Drawing.Size(356, 169);
+            this.txtSMS.TabIndex = 11;
+            // 
+            // SMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ClientSize = new System.Drawing.Size(438, 442);
+            this.Controls.Add(this.guna2GradientButton2);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.guna2GradientPanel1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.ComPort);
+            this.Controls.Add(this.txtSMS);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SMS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SMS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.guna2GradientPanel1.ResumeLayout(false);
+            this.guna2GradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2TextBox txtSMS;
+        private Guna.UI2.WinForms.Guna2ComboBox ComPort;
+        private Guna.UI2.WinForms.Guna2TextBox txtNumber;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblStatus;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2GradientButton btnConnect;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton2;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox btnClose;
+    }
+}
+
---
+++ SendingSMS/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GsmComm.GsmCommunication;
+using GsmComm.PduConverter;
+using GsmComm.Server;
+
+namespace SendingSMS
+{
+    public partial class SMS : Form
+    {
+        private GsmCommMain _GsmCom;
+        public SMS()
+        {
+            InitializeComponent();
+        }
+
+        //----------CODE BY-------------\\
+        // LULITHA GIHAN
+        // lulitha.info@gmail.com
+        // 94753333435
+        //------------------------------//
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Assign Ports
+            ComPort.Items.Add("COM1");
+            ComPort.Items.Add("COM2");
+            ComPort.Items.Add("COM3");
+            ComPort.Items.Add("COM4");
+            ComPort.Items.Add("COM5");
+        }
+
+       
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (ComPort.Text == "")
+            {
+                MessageBox.Show("Invalid Port Name");
+                return;
+            }
+            _GsmCom = new GsmCommMain(ComPort.Text, 9600, 150);
+            Cursor.Current = Cursors.Default;
+            bool retry;
+            do
+            {
+                retry = false;
+                // trying to connect
+                try 
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    _GsmCom.Open();
+
+
+                    MessageBox.Show("Modem Connected Sucessfully..!");
+                    btnConnect.Enabled = false;
+                    lblStatus.Text = "Modem is connected..!";
+                }
+                catch (Exception)
+                {
+                    Cursor.Current = Cursors.Default;
+                    if (MessageBox.Show(this, "GSM Modem is not available", "Check again",
+                        MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning) == DialogResult.Retry)
+                        retry = true;
+                    else
+                    { return; }
+                }
+            }
+            while (retry);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Send SMS
+                Cursor.Current = Cursors.WaitCursor;
+                SmsSubmitPdu _PDU;
+                Cursor.Current = Cursors.Default;               
+                _PDU = new SmsSubmitPdu(txtSMS.Text, txtNumber.Text);
+                int times = 1;
+                for (int i = 0; i < times; i++)
+                {
+                    _GsmCom.SendMessage(_PDU);
+                }
+
+                MessageBox.Show("Message Sent Succesfully..!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
